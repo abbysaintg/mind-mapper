4e332376d02942e4a5fe78a55efc043a35a162b7
Abby St. Germain <abbysaintg@gmail.com>
2023-03-07 11:45:46 -0500

    different cp path

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,7 +5,7 @@ set -o errexit
 # Build commands for front end to create the production build
 rm -rf public
 npm install --prefix client && npm run build --prefix client
-cp -a client/dist/. public/
+cp -a dist/. public/
 
 # Build commands for back end
 bundle install
