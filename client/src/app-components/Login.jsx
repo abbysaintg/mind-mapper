964c05dcdaf756febd7a5f28c67baf84288dc6af
Abby St. Germain <abbysaintg@gmail.com>
2023-03-08 12:23:13 -0500

    user login error handling

--- client/src/app-components/Login.jsx
+++ client/src/app-components/Login.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
-
 function Login({ handleLogin }) {
 	const [email, setEmail] = useState('')
 	const [password, setPassword] = useState('')
+	const [error, setError] = useState(null)
 
 	const handleSubmit = (event) => {
 		event.preventDefault()
@@ -21,34 +21,46 @@ function Login({ handleLogin }) {
 				},
 			}),
 		})
-			.then((response) => {
-				if (response.ok) {
-					return response.json()
+			.then((resp) => {
+				if (resp.ok) {
+					return resp.json()
 				} else {
-					throw new Error('Login failed')
+					throw new Error('Unable to login at this time. Please try again later.')
 				}
 			})
-			.then((data) => handleLogin(data.user))
-			.catch((error) => console.log('Error:', error))
+			.then((data) => {
+				if (data.user) {
+					handleLogin(data.user)
+				} else {
+					throw new Error('Incorrect email or password.')
+				}
+			})
+			.catch((error) => {
+				setError(error.message)
+			})
 	}
 
 	return (
 		<div className="login-form">
-            <form onSubmit={handleSubmit}>
-                <div className="form-input">
-                    <input type="email" value={email} onChange={(event) => setEmail(event.target.value)} required />
-                    <label>Email</label>
-                </div>
-                <div className="form-input">
-                    <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} required />
-                    <label>Password</label>
-                </div>
+
+			<form onSubmit={handleSubmit}>
+				<div className="form-input">
+					<input type="email" value={email} onChange={(event) => setEmail(event.target.value)} required />
+					<label>Email</label>
+				</div>
+				<div className="form-input">
+					<input type="password" value={password} onChange={(event) => setPassword(event.target.value)} required />
+					<label>Password</label>
+				</div>
+                {error && <p className="error-message">{error}</p>}
 				<div className="form-buttons">
 					<button type="submit">LOGIN</button>
-					<Link to="/signup"><button>GO TO SIGN UP</button></Link>
+					<Link to="/signup">
+						<button>GO TO SIGN UP</button>
+					</Link>
 				</div>
-            </form>
-        </div>
+			</form>
+		</div>
 	)
 }
 
