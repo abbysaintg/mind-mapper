e6b058271062e6cfa7e8da4aa1f6b428a8de413d
Abby St. Germain <abbysaintg@gmail.com>
2023-03-09 12:05:43 -0500

    image replace video

--- client/src/app-components/Welcome.jsx
+++ client/src/app-components/Welcome.jsx
@@ -1,4 +1,4 @@
-import MapExampleCompressed from '../MapExampleCompressed.mp4'
+import WelcomeImage from '../WelcomeImage.png'
 
 function Welcome() {
 	return (
@@ -8,7 +8,7 @@ function Welcome() {
 			<p className="welcome-text">
 				With our easy-to-use mind mapping tool, you can create and customize your own mind maps to clarify your thinking and improve your productivity.
 			</p>
-			<video className="welcome-video" src={MapExampleCompressed} autoPlay loop />
+			<img className="welcome-image" src={WelcomeImage} />
 		</div>
 	)
 }
